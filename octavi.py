da540ef46c552fef3f90c028c011a0bf28523cc7
Ken_kaneki_69 <55368020+dhimanparas20@users.noreply.github.com>
2021-04-07 00:00:47 +0530

    Update octavi.py

--- octavi.py
+++ octavi.py
@@ -5,10 +5,10 @@ import time
 def fix (rdir_name):
   return(
     print("-----------------------------------Syncing Private repo----------------------------------------------"),
-    os.system("cd && cd " + rdir_name + " && rm -rf frameworks/base && rm -rf packages/apps/Settings && rm -rf packages/apps/OctaviLab && git clone -b 2.1 https://github.com/Octavi-Broken-Lab/frameworks_base.git frameworks/base && git clone -b 2.1 https://github.com/Octavi-Broken-Lab/android_packages_apps_Settings.git packages/apps/Settings && git clone -b 2.1 https://github.com/Octavi-Broken-Lab/android_packages_apps_OctaviLab.git packages/apps/OctaviLab "),
+    os.system("cd && cd " + rdir_name + " && rm -rf frameworks/base && rm -rf packages/apps/Settings && rm -rf packages/apps/OctaviLab && git clone -b 2.2 https://github.com/Octavi-Broken-Lab/frameworks_base.git frameworks/base && git clone -b 2.2 https://github.com/Octavi-Broken-Lab/android_packages_apps_Settings.git packages/apps/Settings && git clone -b 2.2 https://github.com/Octavi-Broken-Lab/android_packages_apps_OctaviLab.git packages/apps/OctaviLab "),
     os.system("cd && cd " + rdir_name + " && cd packages/apps/Settings/res/drawable/ && cp ic_device_x00td.png x00td.png "),
     os.system("cd && cd " + rdir_name + " && cd packages/apps/Settings/src/com/android/settings/ && rm -rf AboutPhoneData.java "),
-#    os.system("cd && cd " + rdir_name + " && git clone https://bitbucket.org/syberia-project/external_motorola_faceunlock.git -b 11.0 external/motorola/faceunlock "),
+    os.system("cd && cd " + rdir_name + " && git clone https://bitbucket.org/syberia-project/external_motorola_faceunlock.git -b 11.0 external/motorola/faceunlock "),
     os.system("cp AboutPhoneData.java /home/mst/"+rdir_name+"/packages/apps/Settings/src/com/android/settings"),
     print("-----------------------------------Private Repo Synced ----------------------------------------------"),
     print("Now Run command again to build "),
