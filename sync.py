adab27a61beace26e81ae46e158b1c3a08a8fd04
Ken_kaneki_69 <55368020+dhimanparas20@users.noreply.github.com>
2021-04-24 08:50:51 +0530

    Update sync.py

--- sync.py
+++ sync.py
@@ -13,8 +13,7 @@ def disp():
     print ("1: CherishOS"),
     print ("2: Cr-Droid "),
     print ("3: DotOS "),
-    print ("4: LegionOS "),
-    print ("5: Octavi OS"),
+    print ("4: Octavi OS"),
     print ("=================================================="))
     
     
@@ -28,8 +27,7 @@ def opt () :
     print ("1: CherishOS"),
     print ("2: Cr-Droid "),
     print ("3: DotOS "),
-    print ("4: LegionOS "),
-    print ("5: Octavi OS"),
+    print ("4: Octavi OS"),
     print("=================================================="))    
 
 #syncing rom repo
@@ -63,19 +61,9 @@ def repo (inp,rdir):
       print("--------------------------------------------------------------------------------------"),
       time.sleep(3),
       print("Repo Sync Complete "))
-    
+      
   
   elif inp == 4 :
-    #LegionOS  
-    return(
-      os.system("cd && mkdir "+ rdir + " && cd " + rdir + " && repo init --depth=1 -u https://github.com/Project-LegionOS/manifest.git -b 11"),
-      os.system("cd  && cd " + rdir + " && repo sync -c -q --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)"),
-      print("--------------------------------------------------------------------------------------"),
-      time.sleep(3),
-      print("Repo Sync Complete "))    
-    
-  
-  elif inp == 5 :
     #Octavi-OS
     return(
       os.system("cd && mkdir "+ rdir + " && cd " + rdir + " && repo init -u https://github.com/Octavi-OS/platform_manifest.git -b 11"),
@@ -90,11 +78,11 @@ def repo (inp,rdir):
 #syncing device trees
 def tree (b,rdir):
   return(
-    os.system("cd && cd " + rdir + " && git clone https://github.com/ElectroPerf/android_kernel_asus_sdm660.git kernel/asus/sdm660"),
+    os.system("cd && cd " + rdir + " && git clone https://github.com/ElectroPerf/android_kernel_asus_sdm660.git -b X00TD-P-WIFI-STABLE kernel/asus/sdm660"),
     print("--------------------------------------------------------------------------------------"),
     os.system("cd && cd " + rdir + " && git clone https://github.com/dhimanparas20/vendor-asus.git vendor/asus "),
     print("--------------------------------------------------------------------------------------"),
-    os.system("cd && cd " + rdir + " && git clone https://github.com/dhimanparas20/device_asus_sdm660-common.git  device/asus/sdm660-common"),
+    os.system("cd && cd " + rdir + " && git clone https://github.com/dhimanparas20/device_asus_sdm660-common.git device/asus/sdm660-common"),
     print("--------------------------------------------------------------------------------------"),
     os.system("cd && cd " + rdir + " && git clone https://github.com/dhimanparas20/device_asus_X00TD.git -b " + b + " device/asus/X00TD "),
     print("------------------------------------------------------------------------------------------------"))
