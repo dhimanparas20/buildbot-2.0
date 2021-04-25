b28489cc054f25fa627d01ed8eea5aa2cd90f109
Ken_kaneki_69 <55368020+dhimanparas20@users.noreply.github.com>
2021-04-25 07:28:23 +0530

    Update compile.py

--- compile.py
+++ compile.py
@@ -1,31 +1,32 @@
 import time
 import os
-import sync 
+import sync
+import octavi
 
 def run(choice,rdir_name,inp):
   print("==================================================")
   if choice == 1 :
     return(
       os.system("cd && cd buildbot && cp cherish.sh /home/mst/"+ rdir_name ),
-      os.system("cd && cd " + rdir_name + "chmod +x cherish.sh && bash cherish.sh "))
+      os.system("cd && cd " + rdir_name + " chmod +x cherish.sh && bash cherish.sh "))
   elif choice == 2 :
     return(
       os.system("cd && cd buildbot && cp cr.sh /home/mst/"+ rdir_name ),
-      os.system("cd && cd " + rdir_name + "chmod +x cr.sh && bash cr.sh "))
+      os.system("cd && cd " + rdir_name + " chmod +x cr.sh && bash cr.sh "))
   elif choice == 3 :
     return(
       os.system("cd && cd buildbot && cp dot.sh /home/mst/"+ rdir_name ),
-      os.system("cd && cd " + rdir_name + "chmod +x dot.sh && bash dot.sh "))
+      os.system("cd && cd " + rdir_name + " chmod +x dot.sh && bash dot.sh "))
   elif choice == 4 :
     if inp == "y" or inp == "Y" :
       return(
         octavi.fix(rdir_name),
         os.system("cd && cd buildbot && cp octavi.sh /home/mst/"+ rdir_name ),
-        os.system("cd && cd " + rdir_name + "chmod +x octavi.sh && bash octavi.sh "))
+        os.system("cd && cd " + rdir_name + " chmod +x octavi.sh && bash octavi.sh "))
     elif inp == "n"  or inp == "N" :
       return(
         os.system("cd && cd buildbot && cp octavi.sh /home/mst/"+ rdir_name ),
-        os.system("cd && cd " + rdir_name + "chmod +x octavi.sh && bash octavi.sh "))
+        os.system("cd && cd " + rdir_name + " chmod +x octavi.sh && bash octavi.sh "))
     else :
       return(
         time.sleep(1),
