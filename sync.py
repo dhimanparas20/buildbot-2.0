384b17c12903dd147f776c3090e39fa3f3df316a
Ken_kaneki_69 <55368020+dhimanparas20@users.noreply.github.com>
2021-05-22 07:00:53 +0530

    Update sync.py

--- sync.py
+++ sync.py
@@ -80,7 +80,7 @@ def tree (opt,b,rdir):
   if opt == 'N' or opt == 'n':
     return(
       print("/////////////////////////////===CLONING OLD TREES===////////////////////////////////////"),
-      os.system("cd && cd " + rdir + " && git clone https://github.com/dotOS-Devices/kernel_asus_sdm660.git -b X00TD-P-WIFI-STABLE kernel/asus/sdm660"),
+      os.system("cd && cd " + rdir + " && git clone https://github.com/ElectroPerf/android_kernel_asus_sdm660-LTO.git -b X00TD-P-WIFI-STABLE kernel/asus/sdm660"),
       print("--------------------------------------------------------------------------------------"), 
       os.system("cd && cd " + rdir + " && git clone https://github.com/dhimanparas20/vendor-asus.git vendor/asus "),
       print("--------------------------------------------------------------------------------------"),
@@ -92,7 +92,7 @@ def tree (opt,b,rdir):
   elif opt == "Y" or opt == "y":
     return(
       print("/////////////////////////////===CLONING NEW TREES===////////////////////////////////////"),
-      os.system("cd && cd " + rdir + " && git clone https://github.com/dotOS-Devices/kernel_asus_sdm660.git-b X00TD-P-WIFI-STABLE kernel/asus/sdm660"),
+      os.system("cd && cd " + rdir + " && git clone https://github.com/ElectroPerf/android_kernel_asus_sdm660-LTO.git -b X00TD-P-WIFI-STABLE kernel/asus/sdm660"),
       print("--------------------------------------------------------------------------------------"), 
       os.system("cd && cd " + rdir + " && git clone https://github.com/dhimanparas20/nvt.git vendor/asus "),
       print("--------------------------------------------------------------------------------------"),
