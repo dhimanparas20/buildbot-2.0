a462070580e636ac1e9d9895ecac683f506e70a3
Ken_kaneki_69 <55368020+dhimanparas20@users.noreply.github.com>
2021-05-25 05:49:39 -0700

    Update octavi.py

--- octavi.py
+++ octavi.py
@@ -6,7 +6,7 @@ import octavi
 def fix (rdir_name):
   return(
     print("-----------------------------------Syncing Private repo----------------------------------------------"),
-    os.system("cd && cd " + rdir_name + " && rm -rf frameworks/base && rm -rf packages/apps/Settings && rm -rf packages/apps/OctaviLab && rm -rf packages/overlays/Themes && git clone https://github.com/Octavi-Broken-Lab/frameworks_base.git frameworks/base && git clone https://github.com/Octavi-Broken-Lab/android_packages_apps_Settings.git packages/apps/Settings && git clone https://github.com/Octavi-Broken-Lab/android_packages_apps_OctaviLab.git packages/apps/OctaviLab && git clone https://github.com/Octavi-Broken-Lab/packages_overlays_Themes.git packages/overlays/Themes "),
+    os.system("cd && cd " + rdir_name + " && rm -rf frameworks/base && rm -rf packages/apps/Settings && rm -rf packages/apps/OctaviLab && rm -rf packages/overlays/Themes && git clone https://github.com/Octavi-OS/maintainer_fwb.git frameworks/base && git clone https://github.com/Octavi-OS/maintainer_settings.git packages/apps/Settings && git clone https://github.com/Octavi-OS/maintainer_octavilab.git packages/apps/OctaviLab && git clone https://github.com/Octavi-OS/maintainer_themes.git packages/overlays/Themes "),
     os.system("cd && cd " + rdir_name + " && cd packages/apps/Settings/res/drawable/ && rm -rf ic_device_x00td.png "),
     os.system("cd && cd " + rdir_name + " && cd packages/apps/Settings/res/drawable/ && cp daisy.png x00td.png && cp daisy.png ic_device_x00td.png "),     
     os.system("cd && cd " + rdir_name + " && cd packages/apps/Settings/src/com/android/settings/ && rm -rf AboutPhoneData.java "),
