2019297743482ce995e55dfd80562dd6e9b955a7
Ken_kaneki_69 <55368020+dhimanparas20@users.noreply.github.com>
2021-06-24 03:53:20 -0700

    Delete octavi.sh

--- octavi.sh
+++
@@ -1,5 +0,0 @@
-. build/envsetup.sh
-export USE_CCACHE=1 && ccache -M 50G && export CONFIG_STATE_NOTIFIER=y 
-lunch octavi_X00TD-user
-export SKIP_ABI_CHECKS=true && export SKIP_API_CHECKS=true
-brunch X00TD
