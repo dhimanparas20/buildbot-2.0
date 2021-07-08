1768ff85d19b28576e62981faef7b69c7414b191
Ken_kaneki_69 <55368020+dhimanparas20@users.noreply.github.com>
2021-07-08 00:51:45 -0700

    Update sync.py

--- sync.py
+++ sync.py
@@ -16,20 +16,6 @@ def disp():
     print ("3: ", vars.ROM_NAME3),
     print ("4: ", vars.ROM_NAME4),
     print ("=================================================="))
-    
-    
-    
-def opt () :
-  return(
-    print(),
-    print ("------------------------------------------------"),
-    print ("            Choose rom Choice to compile        "),
-    print ("------------------------------------------------"),
-    print ("1: ", vars.ROM_NAME1),
-    print ("2: ", vars.ROM_NAME2),
-    print ("3: ", vars.ROM_NAME3),
-    print ("4: ", vars.ROM_NAME4),
-    print("=================================================="))    
 
 #syncing rom repo
 def repo (inp,rdir):
@@ -71,33 +57,17 @@ def repo (inp,rdir):
       os.system("cd && cd " +rdir+ " &&" +vars.ROM_LINK4),
       print("--------------------------------------------------------------------------------------"),
       time.sleep(3),
-      print("Repo Sync Complete "))       
-    
- 
-     
+      print("Repo Sync Complete ")) 
 
 #syncing device trees
-def tree (opt,b,rdir):
-  if opt == 'N' or opt == 'n':
-    return(
-      print("/////////////////////////////===CLONING OLD TREES===////////////////////////////////////"),
-      os.system("cd && cd " + rdir + " && git clone " +vars.KT+ " " +vars.P_KT),
-      print("--------------------------------------------------------------------------------------"), 
-      os.system("cd && cd " + rdir + " && git clone " +vars.VT+ " " +vars.P_VT),
-      print("--------------------------------------------------------------------------------------"),
-      os.system("cd && cd " + rdir + " && git clone " +vars.OCDT+ " " +vars.P_CDT),
-      print("--------------------------------------------------------------------------------------"),
-      os.system("cd && cd " + rdir + " && git clone " +vars.DT+ " -b " +b+ " " +vars.P_DT),
-      print("------------------------------------------------------------------------------------------------"))
-      
-  elif opt == "Y" or opt == "y":
-    return(
-      print("/////////////////////////////===CLONING NEW TREES===////////////////////////////////////"),
-      os.system("cd && cd " + rdir + " && git clone " +vars.KT+ " " +vars.P_KT),
-      print("--------------------------------------------------------------------------------------"), 
-      os.system("cd && cd " + rdir + " && git clone " +vars.VT+ " " +vars.P_VT),
-      print("--------------------------------------------------------------------------------------"),
-      os.system("cd && cd " + rdir + " && git clone " +vars.NCDT+ " " +vars.P_CDT),
-      print("--------------------------------------------------------------------------------------"),
-      os.system("cd && cd " + rdir + " && git clone " +vars.DT+ " -b " +b+ " " +vars.P_DT),
-      print("------------------------------------------------------------------------------------------------"))
+def tree (b,rdir):
+  return(
+    print("/////////////////////////////===CLONING OLD TREES===////////////////////////////////////"),
+    os.system("cd && cd " + rdir + " && git clone " +vars.KT+ " " +vars.P_KT),
+    print("--------------------------------------------------------------------------------------"), 
+    os.system("cd && cd " + rdir + " && git clone " +vars.VT+ " " +vars.P_VT),
+    print("--------------------------------------------------------------------------------------"),
+    os.system("cd && cd " + rdir + " && git clone " +vars.CDT+ " " +vars.P_CDT),
+    print("--------------------------------------------------------------------------------------"),
+    os.system("cd && cd " + rdir + " && git clone " +vars.DT+ " -b " +b+ " " +vars.P_DT),
+    print("------------------------------------------------------------------------------------------------"))
