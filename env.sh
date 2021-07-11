42558c854ddb45ef1c40c11efb4926a18c8a12f5
Ken_kaneki_69 <55368020+dhimanparas20@users.noreply.github.com>
2021-07-11 07:28:42 +0530

    Add files via upload

---
+++ env.sh
@@ -0,0 +1,22 @@
+#codes for build environment
+
+sudo apt-get update -y
+sudo su 
+echo "============================INSTALLING=================================="
+sudo apt install software-properties-common -y 
+add-apt-repository ppa:openjdk-r/ppa -y
+sudo apt-get install rsync wget git nano screen python curl tmate -y
+sudo apt-get install bison build-essential curl ccache flex lib32ncurses5-dev lib32z1-dev  libncurses5-dev libsdl1.2-dev libxml2 libxml2-utils lzop pngcrush schedtool squashfs-tools xsltproc zip zlib1g-dev git-core make gperf openjdk-8-jdk -y
+sudo apt install bc g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev  libxml2 libxml2-utils lzop pngcrush schedtool squashfs-tools xsltproc zip zlib1g-dev -y
+exit
+mkdir ~/bin
+PATH=~/bin:$PATH
+cd ~/bin
+curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+chmod a+x ~/bin/repo
+git clone https://github.com/akhilnarang/scripts.git scripts && cd scripts
+bash setup/android_build_env.sh
+cd && ls 
+python3 vars.py
+echo "============================DONE=================================="
+./run.sh
