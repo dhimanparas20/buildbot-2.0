bb8fb0cc39e49d54f1f4d976a1d6fbfafdab5e45
Ken_kaneki_69 <55368020+dhimanparas20@users.noreply.github.com>
2021-07-12 19:59:00 -0700

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,22 +1,22 @@
 # place all your variables here 
 # This is the only file you need to edit 
-# Fill the details between the quotes
+# if you dont want to edit this you can also run "python3 entvar.py" to add variables by script
 
 import os
 import time
 
 # Name of server
-SERVER_NAME = "ubuntu"  #change according to your server name
+SERVER_NAME = "mst"
 
 # Github
-EMAIL = ""  # your github email address
-USER_NAME = "" # your github username
+EMAIL = "dhimanparas20@gmail.com"
+USER_NAME = "dhimanparas20"
 
 # Names of roms to sync
-ROM_NAME1 = "CherishOS"  # rom 1 name 
-ROM_NAME2 = "Cr-Droid"  #rom 3 name
-ROM_NAME3 = "DotOS"  #rom 3 name
-ROM_NAME4 = "Octavi OS"  #rom 4 name 
+ROM_NAME1 = "CherishOS"
+ROM_NAME2 = "Cr-Droid"
+ROM_NAME3 = "DotOS"
+ROM_NAME4 = "Octavi OS"
 
 # Repo init commands of the roms
 ROM_LINK1 = "repo init -u https://github.com/CherishOS/android_manifest.git -b eleven && repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags "
@@ -26,18 +26,19 @@ ROM_LINK4 = "repo init -u https://github.com/Octavi-OS/platform_manifest.git -b 
 
 # Links for the trees 
 # Branch will be automatically asked evertime inside script so no need to worry
-DT = "" # device tree
-CDT = ""  # keep empty if you dont have any commmon tree else put common tree link
-VT = ""  # vendor tree
-KT = ""  # kernel tree
+DT = "https://github.com/dhimanparas20/device-asus-X00TD.git" # device tree
+OCDT = "https://github.com/dhimanparas20/device_asus_sdm660-common.git"  # keep empty if you dont have any commmon tree
+NCDT = "https://github.com/dhimanparas20/cdt.git"
+VT = "https://github.com/dhimanparas20/vendor-asus.git"  # vendor tree
+KT = "https://github.com/dotOS-Devices/kernel_asus_X00TD.git"  # kernel tree
 
-# Place where you want trees to be synced 
+# PLce where you want trees to be synced
 P_DT = "device/asus/X00TD"
-P_CDT = ""
-P_VT = ""
-P_KT = ""
+P_CDT = "device/asus/sdm660-common"
+P_VT = "vendor/asus"
+P_KT = "kernel/asus/sdm660"
 
-# Name of SourceForge diractory
+# Name of SourceForge Project
 SF = "mst-2069@frs.sourceforge.net:/home/frs/project/" # needed to upload to sourceforge 
 
 # Name of rclone cloud
@@ -46,6 +47,6 @@ RC = "gd:" # needed to upload to google drive using rclone
 # Name of output Directory
 OUT = "out/target/product/X00TD"  # where the rom zip is placed after rom getting compiled sucessfully
 
-# ===============================DO NOT EDIT AFTER THIS==================================#
-print("===============SAVING GITHUB INFO===========")
+# DO NOT EDIT AFTER THIS 
+print("==================================|SAVING GITHUB INFO|=====================================")
 os.system ("git config --global user.email \"" +EMAIL+ "\" && git config --global user.name \"" +USER_NAME+ "\"")
