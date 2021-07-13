3ad6f89e66c3c94a1fa89a9d80b44294f4faba65
Ken_kaneki_69 <55368020+dhimanparas20@users.noreply.github.com>
2021-07-12 20:03:29 -0700

    Update env.sh

--- env.sh
+++ env.sh
@@ -4,7 +4,7 @@ sudo apt-get update -y
 sudo su 
 echo "============================INSTALLING=================================="
 sudo apt install software-properties-common -y 
-add-apt-repository ppa:openjdk-r/ppa -y
+sudo add-apt-repository ppa:openjdk-r/ppa -y
 sudo apt-get install rsync wget git nano screen python curl tmate -y
 sudo apt-get install bison build-essential curl ccache flex lib32ncurses5-dev lib32z1-dev  libncurses5-dev libsdl1.2-dev libxml2 libxml2-utils lzop pngcrush schedtool squashfs-tools xsltproc zip zlib1g-dev git-core make gperf openjdk-8-jdk -y
 sudo apt install bc g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev  libxml2 libxml2-utils lzop pngcrush schedtool squashfs-tools xsltproc zip zlib1g-dev -y
