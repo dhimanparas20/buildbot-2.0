edb179e65247e281cd26922e86afb8849d20e05f
Ken_kaneki_69 <55368020+dhimanparas20@users.noreply.github.com>
2021-07-12 20:03:04 -0700

    Update code.py

--- code.py
+++ code.py
@@ -1,40 +1,22 @@
 # python function to exit or loop the whole code
+
 import os
 import time
 
 def loop () :
   return(
-    os.system("clear"),
     print(),
     print(),
     print("------------------Returning to main menue------------------------"),
-    os.system("./run.sh"))
+    os.system("python3 go.py"))
     
 def exit () :
   return(  
-    os.system("clear"),
     print(),
     print(),
     print("---------------------Exiting the code------------------------"),   
     os.system("cd && ls"))
-
-def ask (opt) :
-  if opt == 1:
-    return(
-      os.system("clear"),
-      print(),
-      print(),      
-      print("------------------Returning to main menue------------------------"),
-      os.system("./run.sh"))
-      
-  elif opt == 0:
-    return(
-      os.system("clear"),
-      print(),
-      print(),
-      print("---------------------Exiting the code------------------------"),   
-      os.system("cd && ls"))
-      
+     
 def home (rdir) :
   return(
     os.system("cd && cd " + rdir + " && ls "))
